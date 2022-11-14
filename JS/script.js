3a8019a7c497595a095bd75554e1f845c14ea473
Haiquel R. Lawreniuk <108825789+haiquel@users.noreply.github.com>
2022-11-14 03:51:29 -0300

    Terminación del header en servicios

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,11 @@
-var capa = document.getElementById("capa");
-
-function addElemento(texto) {
-    var capa = document.getElementById("capa");
-    var h1 = document.createElement("h1");
-    h1.innerHTML = texto;
-    capa.appendChild(h1);
+var inputs=document.getElementsByClassName('inputform');
+for(var i=0; i<inputs.length; i++){
+    inputs[i].addEventListener('keyup', function(){
+        if(this.value.length>=1){
+            this.nextElement.classList.add('fijar');
+        }
+        else{
+            this.nextElement.classList.remove('fijar');
+        }
+    });
 }
-
-const fragment = document.createDocumentFragment();
-
-for (let i = 0; i < 5000; i++) {
-    const div = document.createElement("div");
-    div.textContent = `Item número ${i}`;
-    fragment.appendChild(div);
-}
-
-document.body.appendChild(fragment);
-
-const div = document.createElement("div"); // <div></div>
-div.id = "page"; // <div id="page"></div>
-div.className = "data"; // <div id="page" class="data"></div>
-div.style = "color: red"; // <div id="page" class="data" style="color: red"></div>
